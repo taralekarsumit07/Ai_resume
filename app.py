74d56b0dc1900187e620b1c11a91795136755020
Sumit <taralekarsumit07@gmail.com>
2026-05-08 21:59:57 +0530

    fixed render port

--- app.py
+++ app.py
@@ -322,4 +322,6 @@ def calculate_match(resume, job):
 # -------- RUN -------- #
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+
+app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
